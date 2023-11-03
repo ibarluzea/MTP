32f4ecc03b42a265758e5984d21640e644c05b02
danicendagorta <128463971+danicendagorta@users.noreply.github.com>
2023-11-03 12:04:14 +0100

    Define compress and decompress

--- ncompress_test.py
+++ ncompress_test.py
@@ -1,29 +1,41 @@
-from ncompress import compress, decompress
-import io
+def compress(msg):
 
-input_text = "EnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezEnekoTelecoMarcIbañezMarcIbañezFINAL!"
-file = open("/content/file.txt", "w", encoding='utf-32')
-file.write(input_text)
-file.close()
+    dictionary = {}
+    output = []
+    buffer = ""
+# Creant el diccionari per UNICODE (amb ascii es de 256) --> NO ESTAN TOTS ELS CARACTERS, PERO ESQUE HI HAN MES DE 100000.
+    for i in range(65536):
+      dictionary[chr(i)] = i
+    next_code = 65536
 
-file = open("/content/file.txt", "rb")
-bytes_file = file.read()
-compressed_text = compress(bytes_file)
-#print(compressed_text)
-print(bytes_file)
+    for i in msg:
+        buffer += i # Miro el seguent caracter i del msg.
+        if buffer not in dictionary:  #Afegeixo al diccionari i al output la seq no guardada -->.
+            output.append(dictionary[buffer[:-1]])
+            dictionary[buffer] = next_code
+            next_code += 1
+            buffer = i
 
-decompressed_text = decompress(compressed_text)
-print(decompressed_text)
-file = open("/content/results.txt", "wb")
-file.write(decompressed_text)
-file.close()
+    output.append(dictionary[buffer]) # String que conforma el output: Llista dels index del diccionari que envio.
+    return output
 
-losses = 33
-losses_text = compressed_text[:(-1*losses)]
-print(losses_text)
+def decompress(compressed_data):
 
-decompressed_text = decompress(losses_text)
-print(decompressed_text)
-file = open("/content/results_with_losses.txt", "wb")
-file.write(decompressed_text)
-file.close()
+    dictionary = {i: chr(i) for i in range(65536)} # Es pot fer també així i queda més compacte, same que el compressor.
+    output = []
+    buffer = ""
+    next_code = 65536
+
+    for j in compressed_data:
+        if j not in dictionary:
+            entry = buffer + buffer[0]  # Si surt això el canal ens fastidia la compressió i no podem descomprimir.
+        else:
+            entry = dictionary[j]
+        output.append(entry)
+        if buffer:
+            dictionary[next_code] = buffer + entry[0]
+            next_code += 1
+        buffer = entry
+
+    return ''.join(output)
+
