b42647e1a543f0bfe40a686f50c46d19b31bfdf7
ibanyezmarc <99355479+ibanyezmarc@users.noreply.github.com>
2023-11-08 12:33:25 +0100

    Create lzw.py para SRI

---
+++ lzw.py
@@ -0,0 +1,45 @@
+def compress(msg):
+
+    dictionary = {}
+    output = []
+    buffer = ""
+# Creant el diccionari per UNICODE (amb ascii es de 256) --> NO ESTAN TOTS ELS CARACTERS, PERO ESQUE HI HAN MES DE 100000.
+    for i in range(1000):
+      dictionary[chr(i)] = i
+    next_code = 1000
+
+
+    for i in msg:
+        buffer += i # Miro el seguent caracter i del msg.
+        if buffer not in dictionary:  #Afegeixo al diccionari i al output la seq no guardada -->.
+            output.append(dictionary[buffer[:-1]])
+            dictionary[buffer] = next_code
+            next_code += 1
+            buffer = i
+
+    output.append(dictionary[buffer]) # String que conforma el output: Llista dels index del diccionari que envio.
+    print(f'Output: {output}')
+    serialized_data = [num.to_bytes(2, byteorder='big') for num in output]
+    return serialized_data
+
+def decompress(compressed_data):
+
+    compressed_data = [int.from_bytes(byte,byteorder='big') for byte in compressed_data]
+
+    dictionary = {i: chr(i) for i in range(1000)} # Es pot fer també així i queda més compacte, same que el compressor.
+    output = []
+    buffer = ""
+    next_code = 1000
+
+    for j in compressed_data:
+        if j not in dictionary:
+            entry = buffer + buffer[0]  # Si surt això el canal ens fastidia la compressió i no podem descomprimir.
+        else:
+            entry = dictionary[j]
+        output.append(entry)
+        if buffer:
+            dictionary[next_code] = buffer + entry[0]
+            next_code += 1
+        buffer = entry
+
+    return ''.join(output)
