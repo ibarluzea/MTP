c41939f8390cf4ac8a97bc7bdc93ca99c96bb61e
danicendagorta <128463971+danicendagorta@users.noreply.github.com>
2023-11-10 12:10:27 +0100

    Update functions_pi.py

--- functions_pi.py
+++ functions_pi.py
@@ -37,7 +37,7 @@ def openFile(path):
     return strF
 
 def writeFile(path, buff):
-    file = open(path+"result.txt","w", 'utf-8')
+    file = open(path+"result.txt","w")
     file.write(buff)
     file.close()
     
