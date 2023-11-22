004a7f4e0d4b125dd5fa98de004e07700f9d4c0a
JosephZnd <146211779+JosephZnd@users.noreply.github.com>
2023-11-22 19:31:55 +0100

    Update functions_nrf24.py

--- functions_nrf24.py
+++ functions_nrf24.py
@@ -34,7 +34,7 @@ def master(nrf, payload):  # count = 5 will only transmit 5 packets
         # "<f" means a single little endian (4 byte) float value.
         start_timer = time.monotonic_ns()  # start timer
         
-        result = nrf.send(buffer, False, 10)
+        result = nrf.send(buffer)
         ii=1
         while not result and limit:
             
