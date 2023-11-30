f95738753385aef60144c855474ba6d18a9a018a
ibanyezmarc <99355479+ibanyezmarc@users.noreply.github.com>
2023-11-30 10:56:59 +0100

    added chardet print in openfile

--- functions_pi.py
+++ functions_pi.py
@@ -29,7 +29,13 @@ def openFile(path):
         try:
             file = open(glob.glob(path+'*.txt')[0],"r", encoding='utf-32')
             strF= file.read()
+            try:
+                codc=check_codec(pth) #now we use path for codec to read more quickly.
+                print("Chardet detected: "+codc)
+            except:
+                print("Chardet failed")
             print("Tried utf-32")
+            
         except:
             try:
                 file = open(glob.glob(path+'*.txt')[0],"r", encoding='utf-16')
