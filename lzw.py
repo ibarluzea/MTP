b3a81bffd714bc51ba73d702d3f0f2b38eecd9b4
JosephZnd <146211779+JosephZnd@users.noreply.github.com>
2023-12-01 18:45:18 +0100

    Update lzw.py

--- lzw.py
+++ lzw.py
@@ -33,7 +33,7 @@ def decompress(compressed_data, codec):
     if codec=="utf-32":
         codec="utf_32"
     else:
-        codec="utf_16"
+        codec="utf_8"
     compressed_data_str = compressed_data.decode(codec)
     compressed_data_index = [int(index) for index in compressed_data_str.split(',')]
     
