4afe0027ca59101bf58bd9ac8cff186a111b1e4e
JosephZnd <146211779+JosephZnd@users.noreply.github.com>
2023-12-11 20:56:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 
-from functions_pi import *
-from functions_nrf24 import *
-from node_nw import *
-#from lzw import *
-import spidev
-import zlib
-from circuitpython_nrf24l01.rf24 import RF24
-# invalid default values for scoping
+from functions_pi import * # ALL FUNCTIONS THAT ARE NOT RELATED TO RF TRANSMISSIONS. (usb path, reading, writing, use leds)
+from functions_nrf24 import * # TX RX FUNCTIONS
+from node_nw import * # NM FUNCTIONS
+#from lzw import * # INITIAL CUSTOM COMPRESSION FUNCTIONS
+import spidev # python module for interfacing with SPI devices
+import zlib # Optimized compression module
+from circuitpython_nrf24l01.rf24 import RF24 # NRF24 module library
 
 if __name__ == "__main__":
 
     SPI_BUS, CSN_PIN, CE_PIN = (None, None, None)
     try: 
-        try:  # on Linux
-            SPI_BUS = spidev.SpiDev()  # for a faster interface on linux
-            CSN_PIN = DigitalInOut(board.D17) # use CE0 on default bus (even faster than using any pin)
-            CE_PIN = DigitalInOut(board.D22)  # using pin gpio22 (BCM numbering)
+        # Here we configure the pins that work with the NRF24 board.
+        SPI_BUS = spidev.SpiDev()  # for a faster interface on linux
+        CSN_PIN = DigitalInOut(board.D17) 
+        CE_PIN = DigitalInOut(board.D22)  # using pin gpio22
 
-        except ImportError:  # on CircuitPython only
-            # using board.SPI() automatically selects the MCU's
-            # available SPI pins, board.SCK, board.MOSI, board.MISO
-            SPI_BUS = board.SPI()  # init spi bus object
-
-            # change these (digital output) pins accordingly
-            CE_PIN = DigitalInOut(board.D4)
-            CSN_PIN = DigitalInOut(board.D5)
-        
         try:
-            led_on([led_green, led_yellow, led_red])
+            led_on([led_green, led_yellow, led_red]) 
             led_off([led_green, led_yellow, led_red])
             print("success in LED and switch setup")
         except:
@@ -40,7 +30,7 @@ if __name__ == "__main__":
         nrf = None
         attempt = 0
         retry_attempts = 20
-
+        # Connection to the board. It normally used to fail once, so this part ensure the correct setup.
         while attempt < retry_attempts:
             try:
                 nrf = RF24(SPI_BUS, CSN_PIN, CE_PIN)
@@ -76,8 +66,8 @@ if __name__ == "__main__":
         fileMRM="/MTP-F23-MRM-A-TX.txt"
 
         print("Choosing mode")
-        
-        isTransmitter, NMode = select_mode(sw_send, sw_txrx, sw_nm, led_yellow, led_green, led_red)
+    
+        isTransmitter, NMode = select_mode(sw_send, sw_txrx, sw_nm, led_yellow, led_green, led_red) # This helps to choose mode and TX/RX
         
         print("Chosen, is TX: {}, is NM: {}".format(isTransmitter, NMode))
         if not NMode:
@@ -142,7 +132,7 @@ if __name__ == "__main__":
         led_off([led_yellow, led_green, led_red])
         print("Transmision finalizada")
 
-        #wait_idle(sw_off)
+        wait_idle(sw_off) # It is ready to safely power off the board.
     
     
     except KeyboardInterrupt:
